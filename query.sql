2a1f8cd938c07313330890fbcd0944d1e97a7d3d
Bugra AYAN* <112655530+bugratr@users.noreply.github.com>
2023-10-13 23:53:12 +0300

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,9 @@
+CREATE TABLE applicants (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    idNumber VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(50) NOT NULL,
+    documentURL VARCHAR(512) NOT NULL,
+    walletAddress VARCHAR(42) NOT NULL
+);
